956d0c93548b48052530ea04b7dcb830038fbf4c
Миша Епринцев <meprincev@bk.ru>
2020-05-04 01:01:43 +0300

    123

---
+++ 2.4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            for (int i = 0; i <= 5; i++, Console.WriteLine())
+            {
+                for (int j = 0; j <= 5 - n; j++)
+                    Console.Write(" " + j);
+                n++;
+            }
+        }
+    }
+}
